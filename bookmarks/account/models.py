4a26935543226af471b9e1f01155042e6c7218e2
nios <rsoin@wp.pl>
2018-08-01 22:09:46 +0200

    New concept of category

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -46,27 +46,19 @@ class Post(models.Model):
         ordering = ('-publish',)
 
 class Category(models.Model):
-    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
     slug = models.SlugField()
-    parent = models.ForeignKey('self',blank=True, null=True ,related_name='children', on_delete=models.CASCADE)
+    
 
-    class Meta:
-        unique_together = ('slug', 'parent',)    #enforcing that there can not be two
-        verbose_name_plural = "categories"       #categories under a parent with same 
-                                                 #slug 
+    def __str__(self)
+        return self.category
 
-    '''def __str__(self):                           # __str__ method elaborated later in
-        full_path = [self.name]                  # post.  use __unicode__ in place of
-                                                 # __str__ if you are using python 2
-        k = self.parent                          
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    subcategory = models.CharField(max_lenght=200)
 
-        while k is not None:
-            full_path.append(k.name)
-            k = k.parent
-
-        return ' / '.join(full_path[::-1])'''
-
-
+    def __str__(self)
+        return self.subcategory   
 
 
     
